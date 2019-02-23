eb66b7ac3b55a148813267b5ed0ae5a03fb73fff
kaiz711 <kaiz711@naver.com>
2019-02-23 18:24:45 +0900

    .

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('accounts.urls')),
